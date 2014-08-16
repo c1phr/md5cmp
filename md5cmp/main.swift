485820c00a7255309418e0791f19b6c9717ccf5e
Ryan Batchelder <rb688@nau.edu>
2014-08-15 17:29:55 -0700

    Initial Commit

---
+++ md5cmp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  md5cmp
+//
+//  Created by Ryan Batchelder on 8/15/14.
+//  Copyright (c) 2014 Ryan Batchelder. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
