9e796fa481e26b990b9db919cc113d366c0e420f
Ryan Batchelder <rb688@nau.edu>
2014-08-15 17:46:57 -0700

    Initial commit

--- md5cmp/md5cmp-Bridging-Header.h
+++ md5cmp/md5cmp-Bridging-Header.h
@@ -1,13 +1,9 @@
 //
-//  main.swift
+//  md5cmp-Bridging-Header.h
 //  md5cmp
 //
 //  Created by Ryan Batchelder on 8/15/14.
 //  Copyright (c) 2014 Ryan Batchelder. All rights reserved.
 //
 
-import Foundation
-
-println("Hello, World!")
-
-let fh = FileHash();
+#import "FileHash.h"
