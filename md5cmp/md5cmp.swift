836813e067b98e620f0b7607251b38ae83a92c31
Ryan Batchelder <rb688@nau.edu>
2014-08-16 23:50:12 -0700

    Divided application logic out into separate file

---
+++ md5cmp/md5cmp.swift
@@ -0,0 +1,38 @@
+//
+//  md5cmp.swift
+//  md5cmp
+//
+//  Created by Ryan Batchelder on 8/16/14.
+//  Copyright (c) 2014 Ryan Batchelder. All rights reserved.
+//
+//  Swift helper functions wrapping the FileHash library
+
+import Foundation
+
+func SingleFileHash(fileName: String) -> String
+{
+    return FileHash.md5HashOfFileAtPath(fileName)
+}
+
+func FileCompare(file1: String, file2: String) -> (success: Bool, file1hash: String, file2hash: String)
+{
+    let file1hash = FileHash.md5HashOfFileAtPath(file1)
+    let file2hash = FileHash.md5HashOfFileAtPath(file2)
+    
+    if (file1hash == file2hash)
+    {
+        return (true, file1hash, file2hash)
+    }
+    return (false, file1hash, file2hash)
+}
+
+func StringCompare(fileName: String, inputHash: String) -> (success: Bool, fileHash: String)
+{
+    let fileHash = FileHash.md5HashOfFileAtPath(fileName)
+    
+    if (fileHash == inputHash)
+    {
+        return (true, fileHash)
+    }
+    return (false, fileHash)
+}
