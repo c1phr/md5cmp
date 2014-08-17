8787d5c956e235c31007c09e24db827d2806938b
Ryan Batchelder <rb688@nau.edu>
2014-08-16 23:40:22 -0700

    Cleaned up single file case and started dividing responsibilities Implemented file comparison Implemented file->string comparison, pending testing

--- md5cmp/main.swift
+++ md5cmp/main.swift
@@ -8,9 +8,43 @@
 
 import Foundation
 
-let fh = FileHash(); //Probably shouldn't leave this here, but it's a note to myself on how to call Obj-C classes
+func SingleFileHash(fileName: String) -> String
+{
+    return FileHash.md5HashOfFileAtPath(fileName)
+}
 
-func main()
+func FileCompare(file1: String, file2: String) -> (success: Bool, file1hash: String, file2hash: String)
+{
+    let file1hash = FileHash.md5HashOfFileAtPath(file1)
+    let file2hash = FileHash.md5HashOfFileAtPath(file2)
+    
+    if (file1hash == file2hash)
+    {
+        return (true, file1hash, file2hash)
+    }
+    return (false, file1hash, file2hash)
+}
+
+func StringCompare(fileName: String, inputHash: String) -> (success: Bool, fileHash: String)
+{
+    let fileHash = FileHash.md5HashOfFileAtPath(fileName)
+    
+    if (fileHash == inputHash)
+    {
+        return (true, fileHash)
+    }
+    return (false, fileHash)
+}
+
+func PrintHelp() -> Void
+{
+    println("Flags: -h: This help menu.")
+    println("       -f: Single file mode, md5cmp -f <file> returns the MD5 sum of the file")
+    println("       -c: File Compare mode, md5cmp -c <file 1> <file 2> returns if the two files have the same MD5 sum")
+    println("       -s: String Compare mode, md5cmp -s <file> <string> returns if the file's MD5 matches the input string")
+}
+
+func start()
 {
     if (C_ARGC < 2) //No flags, exit
     {
@@ -18,32 +52,55 @@ func main()
         return;
     }
     let args = NSProcessInfo.processInfo().arguments as NSArray
-    let flag = args[0] as String
+    let flag = args[1] as String
     
     switch flag
     {
         case "-h":
-            println("Flags: -h: This help menu.")
-            println("       -f: Single file mode, md5cmp -f <file> returns the MD5 sum of the file")
-            println("       -c: File Compare mode, md5cmp -c <file1> <file2> returns if the two files have the same MD5 sum")
-            println("       -s: String Compare mode, md5cmp -s <file> <string> returns if the file's MD5 matches the input string")
+            PrintHelp()
+        
         case "-f":
-            println("Checking for file")
             if (args.count != 3)
             {
                 println("USAGE: md5cmp -f <file>")
-                return;
+                exit(1)
             }
             else
             {
-                let file: String = args[2] as String
-                let hash = FileHash.md5HashOfFileAtPath(file)
-                println("Hash of " + file)
+                let file = args[2] as String
+                println("Hash of " + file + ":")
+                println(SingleFileHash(file))
             }
+        
+        case "-c":
+            if (args.count != 4)
+            {
+                println("USAGE: md5cmp -c <file 1> <file 2>")
+                exit(1)
+            }
+            else
+            {
+                let file1 = args[2] as String
+                let file2 = args[3] as String
+                let result = FileCompare(file1, file2)
+                if (result.success)
+                {
+                    println(result.file1hash + " == " + result.file2hash)
+                    println("SUCCESS: The files are equal")
+                    exit(0)
+                }
+                else
+                {
+                    println("WARNING: The files are not equal")
+                    println("File 1: " + result.file1hash)
+                    println("File 2: " + result.file2hash)
+                }
+            }
+        
         default:
             println("Invalid Flag: " + flag)
     }
     
 }
 
-main()
+start()
